fe8ddb84c0308a24ab4d0f588922dc4ecff4593b
m77so <m77code@gmail.com>
2023-02-25 15:54:56 +0900

    svelte first map

---
+++ svelte-maprote/src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import Map from './lib/Map.svelte'
+
+
+</script>
+
+<main>
+
+  <Map />
+
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+  #map {
+    height: 800px;
+    width: 1000px;
+  }
+</style>
---
+++ svelte-maprote/src/lib/Map.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import L from 'leaflet'
+    import 'leaflet/dist/leaflet.css';
+    let map: L.Map;
+    function createMap(container: string | HTMLElement): L.Map {
+      let m = L.map(container).setView([43.05, 141.25], 13)
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+      }).addTo(m)
+      return m
+    }
+    function mapAction(container) {
+      map = createMap(container);
+      return {
+        destroy: () => {
+          map.remove();
+        },
+      };
+    }
+  
+  </script>
+  
+    <div class="map" use:mapAction></div>
+
+  
+  <style>
+    .map {
+      height: 1000px;
+      width: 1000px;
+    }
+  </style>
